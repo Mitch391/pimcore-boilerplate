c4f16e6056c8618eb04ba58ff4ab5a176cb8c34c
Michel <m.vanhoof@youwe.nl>
2019-05-20 16:22:42 +0200

    add docker stuff

--- update-pimcore-version.sh
+++ update-pimcore-version.sh
@@ -10,4 +10,17 @@ line=$(grep \"pimcore/pimcore\":\ \" composer.json)
 version=$(echo $line | cut -d '"' -f 4)
 sed "s/$version/$1/g" .composer-base.json > composer.json
 
+#Installing composer packages
 docker-compose exec php bash -c "COMPOSER_MEMORY_LIMIT=-1 composer update --no-suggest --no-interaction --dev -vvv"
+
+#Installing pimcore
+docker-compose exec php bash -c "vendor/bin/pimcore-install --env=dev --ignore-existing-config --no-interaction -vvv"
+
+#Installing assets
+docker-compose exec php bash -c "bin/console assets:install --env=dev --symlink -vvv"
+
+#clearing symfony cache
+docker-compose exec php bash -c "bin/console cache:clear --env=dev --no-interaction --no-warmup -vvv"
+
+#clearing pimcore cache
+docker-compose exec php bash -c "bin/console pimcore:cache:clear --env=dev --no-interaction -vvv"
